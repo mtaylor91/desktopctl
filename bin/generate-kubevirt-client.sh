2ef35cc01c6247227b0a5c8b9a83d62a3dd452ce
Mike Taylor <mike.charles.taylor@gmail.com>
2023-08-18 11:20:41 -0600

    Fix generate-kubevirt-client.sh (trim package path prefix)

--- bin/generate-kubevirt-client.sh
+++ bin/generate-kubevirt-client.sh
@@ -3,5 +3,6 @@ client-gen \
   --input-base="kubevirt.io/api/" \
   --input="core/v1" \
   --output-package="github.com/mtaylor91/desktopctl/pkg/kubevirt/client" \
+  --trim-path-prefix="github.com/mtaylor91/desktopctl/" \
   --clientset-name="versioned" \
   --go-header-file boilerplate.go.txt
